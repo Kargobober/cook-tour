18b7389fc55bb088f0ea27b42964bbf77065cea7
Kargobober <kargobober@yandex.ru>
2024-04-10 01:20:56 +0300

    finish with accordeon component

--- task-js/component-1/js/script.js
+++ task-js/component-1/js/script.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   /**
    * Служебная функция для считывания параметров из адресной строки
    * и определения конфигурации компонента
@@ -22,10 +22,63 @@
    * например: ?tabs_limit=1
    */
   const settings = {
-    tabsLimit: getUrlValue('tabs_limit') || 0,
+    tabsLimit: getUrlValue('tabs_limit') || 1,
   };
 
   /* Код компонента пишите ниже */
 
+  const classForIndicator = 'accordeon-item--open';
+  const items = Array.from(document.querySelectorAll('.accordeon-item')).map((item) => ({
+    markup: item,
+    openingTime: 0,
+  }));
+  let amountOfOpened = 0;
 
+  function getAmountOfOpened() {
+    return items.reduce((acc, li) => {
+      if (li.markup.classList.contains(classForIndicator)) {
+        acc++;
+      }
+      return acc;
+    }, 0);
+  }
+
+  const toggleItem = (togglableItem) => () => {
+    // переключаем наличие класса
+    togglableItem.markup.classList.toggle(classForIndicator);
+
+    // если после переключения класс есть...
+    if (togglableItem.markup.classList.contains(classForIndicator)) {
+      // то элемент открывается, запишем таймстамп его открытия
+      const currentDate = new Date();
+      togglableItem.openingTime = currentDate.getTime();
+    } else {
+      // иначе элемент закрывается, обнуляем дату его открытия
+      togglableItem.openingTime = 0;
+    }
+
+    // обновляем счётчик открытых табов
+    amountOfOpened = getAmountOfOpened();
+
+    // обработаем превышение открытий
+    if (settings.tabsLimit && amountOfOpened > settings.tabsLimit) {
+      const currentDate = new Date();
+      const indexOfOldest = items.reduce(
+        (result, item, index) => {
+          if (item.openingTime > 0 && item.openingTime < result.time) {
+            result.time = item.openingTime;
+            result.indexOfOldest = index;
+          }
+          return result;
+        },
+        { time: currentDate.getTime(), indexOfOldest: null }
+      ).indexOfOldest;
+
+      toggleItem(items[indexOfOldest])();
+    }
+  };
+
+  items.forEach((li) => {
+    li.markup.querySelector('.accordeon-item-title').addEventListener('click', toggleItem(li));
+  });
 })();
